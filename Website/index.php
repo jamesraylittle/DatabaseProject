c26b9674c6d2fba601d9fb84eac5656ca8266e15
James Little <wizkid916@yahoo.com>
2015-04-07 19:57:20 -0500

    Added basic page rendering framework

--- Website/index.php
+++ Website/index.php
@@ -1,4 +1,16 @@
 <?php
 
-    echo "Test";
+    if(isset($_GET['page']))
+        $page = $_GET['page'];
+    if(!isset($page) || $page == "head" || $page == "foot" || $page == "menu" || empty($page))
+        $page = "home";
+    $file = "views/".$page.".php";
+    if(file_exists($file))
+        include($file);
+    else
+        include("views/error404.php");
+
+
+
+
 
---
+++ Website/views/error404.php
@@ -0,0 +1,8 @@
+<?php
+    $title = "404 - Page Not Found.";
+    include("head.php");
+?>
+    <p>The page you are looking for does not exist.</p>
+<?php
+    include("foot.php");
+?>
