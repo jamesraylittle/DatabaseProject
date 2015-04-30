096fcc52b42284ae9e28f9b9798fd993f1c42d56
Sandesh <sandesh.bhattarai@ttu.edu>
2015-04-30 14:11:29 -0500

    minor fix for staff
    minor fix for staff

--- Website/views/professor/add_textbook.php
+++ Website/views/professor/add_textbook.php
@@ -1,15 +1,46 @@
 <?php
+    $page = new Page("Add Textbook");
+
+
+    if(isset($_POST['submit'])) { //The Submit button has been pressed. Process the form.
+        //Making assumptions about if the values are set and are "valid".
+        $instructor_id = $_POST["instructor_id"];
+        $course_id = $_POST["course_id"];
+        $book_title = $_POST["book_title"];
+        $author = $_POST["author"]; 
+        $edition = $_POST["edition"];
+        $isbn = $_POST["isbn"]; 
+        $publisher = $_POST["publisher"];
+
+        $DB->execute(
+            "INSERT INTO Textbook ( instructor_id, course_id, book_title, author, edition, isbn, publisher) VALUES(
+            '".$instructor_id."',
+            '".$course_id."',
+            '".$book_title."',
+            '".$author."',
+            '".$edition."',
+            '".$isbn."',
+            '".$publisher."'
+            )"
+
+  );
+
+
+        $page->addQuery("message", "Textbook has been added.");
+        $page->redirectToMenu();
+}
+    else { //No Post, display page.
     $page = new Page("Add a Textbook");
     $page->showHeader();
 
     echo newForm(
         "add_textbook",
-        $page->getCurrentPage(),
-        "Add Textbook",
+        $page->getPage(),
+        "Add a Textbook",
         array(
             formItem(1, "Instructor's ID", "instructor_id"),
-            formItem(2, "Section ID", "section_id"),
-            formItem(3, "Book Title", "title"),
+            formItem(2, "Course ID", "course_id"),
+            formItem(3, "Book Title", "book_title"),
             formItem(4, "Author", "author"),
             formItem(5, "Edition", "edition"),
             formItem(6, "ISBN Number", "isbn"),
@@ -18,4 +49,25 @@
     );
 
         $page->showFooter();
+    }
+
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Website/views/staff/course_catalog.php
+++ Website/views/staff/course_catalog.php
@@ -1,4 +1,24 @@
 <?php
+    $page = new Page("Select Position");
+
+
+    if(isset($_POST['submit'])) { //The Submit button has been pressed. Process the form.
+        //Making assumptions about if the values are set and are "valid".
+        $semester = $_POST["semester"];
+        
+        $DB->execute(
+            "INSERT INTO Course_Sections (course_id, section_id, semester) VALUES(
+            '".$course_id."',
+            '".$section_id."',
+            '".$semester."'
+            )"
+
+  );
+
+        $page->addQuery("message", "Instructor has been assigned.");
+        $page->redirectToMenu();
+}
+    else { //No Post, display page.
     $page = new Page("Course Catalog");
     $page->showHeader();
 
@@ -7,10 +27,13 @@
         $page->getPage(), //Form Action
         "Course Catalog Year", //Title
         array(
-            formItem(1, "Course", "course"), //form input field
-            formItem(2, "Catalog Year", "catalog_year") //Form input field
+            formItem(1, "Catalog Year", "catalog_year") //Form input field
         )
     );
 
     $page->showFooter();
+    }
+
 ?>
+
+
